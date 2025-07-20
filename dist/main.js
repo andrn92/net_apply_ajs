050a2663ebcd411930692581424435b376d8b38b
andrn <01.nmeml@ya.ru>
2025-07-20 13:20:21 +0300

    Revert "remove add dist"
    This reverts commit 8d3f6ff043866e97ca85ff1b18c7e6c21e9fa221.

--- dist/main.js
+++
@@ -1,1 +0,0 @@
-(()=>{var t={285:function(t){t.exports=function(){"use strict";var t={};return t.default={info:function(){const t=new Map;return t.set("name","ajs"),t.set("version","1.0.0"),t}},t.default}()}},n={};const r=function r(e){var o=n[e];if(void 0!==o)return o.exports;var s=n[e]={exports:{}};return t[e].call(s.exports,s,s.exports,r),s.exports}(285);console.log(r.info())})();
